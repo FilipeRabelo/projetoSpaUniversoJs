68a2f0230377bbb384a58229f75efb3f969bf468
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2023-12-22 10:58:05 -0300

    responsivo

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 import {Router} from './router.js';
 
+const router = new Router();
+router.add('/', "/pages/home.html");
+router.add('/paginaUniverso', '/pages/paginaUniverso.html');
+router.add('/paginaExploracao', '/pages/paginaExploracao.html');
+router.add(404, '/pages/404.html');
 
+router.handle();
+
+window.onpopstate = () => router.handle();
+
+window.route = () => router.route();
 
 document.addEventListener('DOMContentLoaded',function (){
   var caminhoAtual = window.location.pathname;
@@ -17,26 +27,3 @@ document.addEventListener('DOMContentLoaded',function (){
   }
 
 });
-
-
-const router = new Router();
-router.add('/', "/pages/home.html");
-router.add('/paginaUniverso', '/pages/paginaUniverso.html');
-router.add('/paginaExploracao', '/pages/paginaExploracao.html');
-router.add(404, '/pages/404.html');
-
-router.handle();
-
-window.onpopstate = () => router.handle();
-
-window.route = () => router.route();
-
-
-
-
-
-
-
-
-
-
