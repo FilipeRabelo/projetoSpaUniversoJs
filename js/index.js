f12d7c6e98778fd3d14bec5788f1ce2a258098db
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2023-12-22 10:36:31 -0300

    1.0

--- js/index.js
+++ js/index.js
@@ -1,5 +1,24 @@
 import {Router} from './router.js';
 
+
+
+document.addEventListener('DOMContentLoaded',function (){
+  var caminhoAtual = window.location.pathname;
+
+  var links = document.querySelector("nav a");
+
+  links.forEach(function (link){
+    link.classList.remove('ativo')
+  });
+
+  var linkAtivo = document.querySelector('nav a[href="' + caminhoAtual + '"]');
+  if(linkAtivo){
+    linkAtivo.classList.add('ativo');
+  }
+
+});
+
+
 const router = new Router();
 router.add('/', "/pages/home.html");
 router.add('/paginaUniverso', '/pages/paginaUniverso.html');
@@ -11,3 +30,13 @@ router.handle();
 window.onpopstate = () => router.handle();
 
 window.route = () => router.route();
+
+
+
+
+
+
+
+
+
+
